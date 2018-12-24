3d39fb87de1592b00b50d89d6634cdc4ee047092
KadekYuda <yudaikadek22@gmail.com>
2018-12-24 09:02:28 +0700

    Completed main content

--- resto_app/__init__.py
+++ resto_app/__init__.py
@@ -32,5 +32,9 @@ def create_app(test_config=None):
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
     return app
